3fcb08dadbddef820296d83ad871f4209b3e883c
Jason Mayo <jason@bymayo.co.uk>
2018-05-25 14:48:28 +0100

    Updated Icons & Branding

--- commercewidgets/CommerceWidgetsPlugin.php
+++ commercewidgets/CommerceWidgetsPlugin.php
@@ -39,12 +39,12 @@ class CommerceWidgetsPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.0.6';
+        return '1.0.7';
     }
 
     public function getSchemaVersion()
     {
-        return '1.0.6';
+        return '1.0.7';
     }
 
     public function getDeveloper()
