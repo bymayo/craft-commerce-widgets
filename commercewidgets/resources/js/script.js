984f6ec76f9ad15e00c282cca573b4c656778bce
Pete Eveleigh <pete@fantasticmachine.co.uk>
2018-09-04 17:33:48 +0100

    Update script.js
    Make it possible to create more than 1 abandoned cart widget by not using the element's ID to initialise the chart. Could do with also removing the ID from the HTML but I've left that in for now.

--- commercewidgets/resources/js/script.js
+++ commercewidgets/resources/js/script.js
@@ -12,42 +12,45 @@ $(function() {
 	/* Chart - Doghnut */
 	
 	if ($('.cartabandonment').length) {
+		$('.cartabandonment').each(function(){
 
-		var chartData = $('#cart-abandonment-chart').data('chart'),
-			chartDataArray = chartData.split(','),
-			chart = document.getElementById('cart-abandonment-chart');
-		
-		var options = {
-			cutoutPercentage: (chartData == '0,0') ? 0 : 50,
-			tooltips: {
-				enabled: false
+			var chartData = $(this).find($('[data-chart]')).data('chart'),
+				chartDataArray = chartData.split(','),
+				//chart = document.getElementById('cart-abandonment-chart');
+				chart = $(this).find('.chart');
+				
+			var options = {
+				cutoutPercentage: (chartData == '0,0') ? 0 : 50,
+				tooltips: {
+					enabled: false
+				}
 			}
-		}
 
-		var data = {
-		    labels: false,
-		    datasets: [
-		        {
-		            data: (chartData == '0,0') ? [0,1] : chartDataArray,
-		            backgroundColor: [
-		                "#6CBC15",
-		                "#EA9305"
-		            ],
-		            hoverBackgroundColor: [
-		                "#6CBC15",
-		                "#EA9305"
-		            ]
-		        }
-			]
-		};	
-		
-		var myDoughnutChart = new Chart(
-			chart, {
-		    	type: 'doughnut',
-				data: data,
-				options: options
-			}
-		);
+			var data = {
+				labels: false,
+				datasets: [
+					{
+						data: (chartData == '0,0') ? [0,1] : chartDataArray,
+						backgroundColor: [
+							"#6CBC15",
+							"#EA9305"
+						],
+						hoverBackgroundColor: [
+							"#6CBC15",
+							"#EA9305"
+						]
+					}
+				]
+			};	
+			
+			var myDoughnutChart = new Chart(
+				chart, {
+					type: 'doughnut',
+					data: data,
+					options: options
+				}
+			);
+		});
 	
 	}
 
