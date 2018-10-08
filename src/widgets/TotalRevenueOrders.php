8cc860aad3c6534ee6d05a00ad6c6735cb82b94b
Jason Mayo <jason@bymayo.co.uk>
2018-10-08 21:55:40 +0100

    Total Revenue & Orders Widget

--- src/widgets/TotalRevenueOrders.php
+++ src/widgets/TotalRevenueOrders.php
@@ -16,14 +16,17 @@ use bymayo\commercewidgets\assetbundles\commercewidgets\CommerceWidgetsAsset;
 use Craft;
 use craft\base\Widget;
 use craft\helpers\StringHelper;
+use craft\helpers\DateTimeHelper;
+use craft\i18n\Formatter;
+use craft\i18n\Locale;
+use craft\db\Query;
+use craft\records\Session;
 
 class TotalRevenueOrders extends Widget
 {
 
     // Public Properties
     // =========================================================================
-
-    public $limit = 10;
 
     // Static Methods
     // =========================================================================
@@ -43,37 +46,121 @@ class TotalRevenueOrders extends Widget
         return null;
     }
 
+    // Custom Public Methods
+    // =========================================================================
+
+    public function getTimeFrames()
+    {
+
+      return array(
+         array(
+            'label' => 'Today',
+            'date' => date('d M Y', strtotime('monday this week'))
+         ),
+         array(
+            'label' => 'Week',
+            'date' => date('d M Y', strtotime('monday this week')) . ' - ' . date('d M Y', strtotime('sunday this week'))
+         ),
+         array(
+            'label' => 'Month',
+            'date' => date('M')
+         ),
+         array(
+            'label' => 'Year',
+            'date' => date('Y')
+         ),
+         array(
+            'label' => 'All',
+            'date' => '∞'
+         )
+      );
+
+   }
+
+   public function getRevenueOrdersRow($timeFrame)
+   {
+
+      try {
+
+         $query = (
+            new Query()
+            )
+            ->select(
+               [
+                  'COALESCE(sum(orders.totalPrice), 0) as totalRevenue',
+                  'COALESCE(count(orders.id), 0) as totalOrders'
+               ]
+            )
+            ->from(['orders' => 'commerce_orders'])
+            ->where(
+               [
+                  'orders.isCompleted' => 1
+               ]
+            );
+
+         switch ($timeFrame['label']) {
+            case "Today":
+               $query->andWhere(
+                  [
+                     'DATE_FORMAT(orders.dateCreated, "%Y-%m-%d")' => date('Y-m-d')
+                  ]
+               );
+               break;
+            case "Week":
+               $query->andWhere(
+                  [
+                     'WEEK(orders.dateCreated)' => date('W'),
+                     'YEAR(orders.dateCreated)' => date('Y')
+                  ]
+               );
+               break;
+            case "Month":
+               $query->andWhere(
+                  [
+                     'MONTH(orders.dateCreated)' => date('M'),
+                     'YEAR(orders.dateCreated)' => date('Y')
+                  ]
+               );
+               break;
+            case "Year":
+               $query->andWhere(
+                  [
+                     'YEAR(orders.dateCreated)' => date('Y')
+                  ]
+               );
+               break;
+         }
+
+         $result = $query->one();
+
+         return $result;
+
+      }
+      catch (Exception $e) {
+         $result = null;
+      }
+
+   }
+
+   public function getRevenueOrders()
+   {
+
+      $data = array();
+
+      foreach ($this->getTimeFrames() as $timeFrame) {
+         array_push($data, $this->getRevenueOrdersRow($timeFrame));
+      }
+
+      return $data;
+
+   }
+
     // Public Methods
     // =========================================================================
 
     public function getTitle(): string
     {
       return 'Total Revenue & Orders';
-    }
-
-    public function rules()
-    {
-        $rules = parent::rules();
-
-        $rules = array_merge(
-            $rules,
-            [
-                ['limit', 'integer'],
-                ['limit', 'default', 'value' => 10],
-            ]
-        );
-
-        return $rules;
-    }
-
-    public function getSettingsHtml()
-    {
-        return Craft::$app->getView()->renderTemplate(
-            'commerce-widgets/widgets/' . StringHelper::basename(get_class($this)) . '/settings',
-            [
-                'widget' => $this
-            ]
-        );
     }
 
     public function getBodyHtml()
@@ -83,7 +170,8 @@ class TotalRevenueOrders extends Widget
         return Craft::$app->getView()->renderTemplate(
             'commerce-widgets/widgets/' . StringHelper::basename(get_class($this)) . '/body',
             [
-                'limit' => $this->limit
+                'revenueOrders' => $this->getRevenueOrders(),
+                'timeFrames' => $this->getTimeFrames()
             ]
         );
     }
